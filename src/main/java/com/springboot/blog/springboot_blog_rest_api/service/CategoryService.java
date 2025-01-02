46ae25ffc24379a34a05cff7d4be1248e3688a03
TaiNgonheng <heng68807@gmail.com>
2025-01-02 10:25:47 +0700

    CategoryService for inteface that controller to impl

---
+++ src/main/java/com/springboot/blog/springboot_blog_rest_api/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.springboot.blog.springboot_blog_rest_api.service;
+
+import com.springboot.blog.springboot_blog_rest_api.dto.CategoryDto;
+
+public interface CategoryService {
+    CategoryDto addCategory(CategoryDto categoryDto);
+}
