57ae520992060dd87e8cb9eee535826944a65b42
TaiNgonheng <heng68807@gmail.com>
2025-01-04 09:45:16 +0700

    Insert Metadata to Database

--- src/main/java/com/springboot/blog/springboot_blog_rest_api/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/springboot_blog_rest_api/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.springboot.blog.springboot_blog_rest_api;
 
+import com.springboot.blog.springboot_blog_rest_api.entity.Role;
+import com.springboot.blog.springboot_blog_rest_api.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,20 +24,15 @@ import org.springframework.context.annotation.Bean;
 						name = "Tai Ngonheng",
 						email = "heng68807@gmail.com",
 						url = "https://me-portfolio-heng.vercel.app/"
-				),
-
-				license = @License(
-						name = "Apache 2.0",
-						url = "https://www.apache.org/licenses/LICENSE-2.0"
 				)
 		),
 		externalDocs = @ExternalDocumentation(
-				description = "Spring boot blog app Documentation",
+				description = "Spring boot blog app Documentation Github",
 				url = "https://github.com/TaiNgonheng/Real_time_blog_app"
 
 		)
 )
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -43,5 +42,17 @@ public class SpringbootBlogRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
+	@Autowired
+	private RoleRepository roleRepository;
 
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("USER_ROLE");
+		roleRepository.save(userRole);
+	}
 }
